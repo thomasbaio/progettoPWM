14da6f7e4290e53fd5aeb96dd9736baae88b1441
thomasbaio <thomas.baiocchi@studenti.unimi.it>
2025-07-06 22:43:38 +0200

    aggiornare pubbli/marvel.js

--- public/scripts/marvel.js
+++ public/scripts/marvel.js
@@ -52,46 +52,49 @@ async function getDuplicatedAlbumcardsDB(albumID) {
 }
 
 async function printPackage() {
-await getPackage()
-.then(response =>
-    {  printCredits();
-         var i=0;
-            var Div_Car = `<div class="row">
-                        <div class="col-md-12 text-center"> `
-            response.forEach(item => {
-                if (i % 3 ==0 ) {
-                    Div_Car = Div_Car + 
-                    `   </div>
+    await getPackage()
+    .then(response => {
+        printCredits();
+        var i = 0;
+        var Div_Car = `<div class="row">
+                        <div class="col-md-12 text-center"> `;
+        // ASSICURATI DI USARE SEMPRE response.data.results
+        let results = [];
+        if (response && response.data && Array.isArray(response.data.results)) {
+            results = response.data.results;
+        }
+        results.forEach(item => {
+            if (i % 3 == 0) {
+                Div_Car = Div_Car + 
+                `   </div>
                     </div>
                     <div class="row">
                         <div class="col-md-12 text-center"> `;
-                }
-                Div_Car =
-                    Div_Car + 
-                    '<div class="card card-shine-effect-metal" id="char-'+item.data.results[0].id+'">'+
-                        '<div class="card-header">'+
-                            item.data.results[0].name+
-                        '</div>'+
-                        //'<hr>'+
-                        '<div class="card-content">'+
-                            '<img src="'+item.data.results[0].thumbnail.path.replace(/"/g, "")+'.'+item.data.results[0].thumbnail.extension+'">'+
-                        '</div>'+
-                        '<div class="card-body">'+
-                        item.data.results[0].description+
-                        '</div>'+
-                        '<div class="card-footer">'+
-                        item.attributionText+
-                        '</div>'+
-                    '</div>';
-                    i++;
-            });
-            Div_Car = Div_Car + `   </div>
+            }
+            Div_Car =
+                Div_Car + 
+                '<div class="card card-shine-effect-metal" id="char-'+item.id+'">'+
+                    '<div class="card-header">'+
+                        item.name+
+                    '</div>'+
+                    '<div class="card-content">'+
+                        '<img src="'+item.thumbnail.path.replace(/"/g, "")+'.'+item.thumbnail.extension+'">'+
+                    '</div>'+
+                    '<div class="card-body">'+
+                    item.description+
+                    '</div>'+
+                    '<div class="card-footer">'+
+                    'Data provided by ©Marvel'+
+                    '</div>'+
+                '</div>';
+            i++;
+        });
+        Div_Car = Div_Car + `   </div>
                                 </div>
                                 <button onclick=window.location.reload(); class="btn btn-block btn-success w-100">OK</button>`;
-            document.getElementById("pack_cards").innerHTML = Div_Car;
-        }
-)
-.catch(response => console.error("Calculation error!"+response)) 
+        document.getElementById("pack_cards").innerHTML = Div_Car;
+    })
+    .catch(response => console.error("Calculation error!"+response)) 
 }
 
 async function printAlbumCards(albumId) {
